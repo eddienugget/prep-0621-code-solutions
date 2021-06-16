5d5bff17490148a4df26b7f442901e80e6d7259d
norman silvera <amoishere4u@gmail.com>
2021-06-15 18:47:23 -0700

    completed prep-javascript-arrays

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,12 @@
+const languages = [
+  'html', 'css', 'javascript', 'react', 'node', 'postgres'
+];
+console.log(languages);
+var firstElement = languages[0]
+console.log('The first language is', firstElement);
+var thirdElement = languages[2]
+console.log('The third language is', thirdElement);
+var length = languages.length
+console.log('The length of the array is', languages.length)
+var lastElement = languages[5]
+console.log('The last language of the array is', lastElement)
